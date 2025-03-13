6431f2f9b2842ed58a320c842afa32b6a2db46bf
furiousknight25 <48182589+furiousknight25@users.noreply.github.com>
2025-03-13 00:00:44 -0500

    basic framework

--- src/App.java
+++ src/App.java
@@ -1,5 +1,40 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+
     }
+
+    class writer {
+        public void writer(String inputFileName, Integer rows, Integer columns) {
+            
+            
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(inputFileName))) {
+                writer.write("Hello World");
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    class reader {
+        public void reader(String inputFileName) {
+            try (BufferedReader Reader = new BufferedReader(new FileReader(inputFileName))) {
+                String line;
+                while ((line = Reader.readLine()) != null) {
+                    System.out.println(line);
+                }
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
 }
